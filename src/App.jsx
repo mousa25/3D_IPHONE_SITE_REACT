2d00f74ff9e34f388d8a6bf9132f2db95179ace0
mousa25 <mousamohamad943@gmail.com>
2023-07-04 19:53:37 +0200

    commetn

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import SoundSection from "./components/SoundSection";
+import Loader from "./components/Loader";
+import { useRef } from "react";
+function App() {
+  const webgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  };
+  return (
+    <div className="App">
+      <Loader />
+      <div ref={contentRef} id="content">
+        <Nav />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViewer contentRef={contentRef} ref={webgiViewerRef} />
+    </div>
+  );
+}
+
+export default App;
